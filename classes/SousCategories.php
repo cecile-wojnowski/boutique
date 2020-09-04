078bb056b8584016e4096aadb18fea87151751bd
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-04 16:35:53 +0200

    Attributs et méthodes + héritage

---
+++ classes/SousCategories.php
@@ -0,0 +1,13 @@
+<?php
+class SousCategories extends Categories
+{
+  # La classe SousCategories hérite des méthodes de Categories
+  # Mais elle doit avoir un nom et une liste de produits différents
+  private $nom;
+  private $liste_produits;
+
+  public function afficher_categorie_parente(){
+    # Récupère le nom de la catégorie à laquelle appartient la sous-catégorie
+  }
+}
+ ?>
