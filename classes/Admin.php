dc13ae864d508d79b537634aad48f91af320473e
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-04 16:16:14 +0200

    Création des méthodes

---
+++ classes/Admin.php
@@ -0,0 +1,35 @@
+<?php
+class Admin extends Utilisateurs # La classe Admin hérite des propriétés de la classe Utilisateurs
+{
+  /****** Gestion des produits ******/
+  public function ajouter_produit(){
+    # Ajoute un produit dans le site sans nécessairement le mettre dans une catégorie
+  }
+  public function valoriser_produit(){
+    # Permet de mettre en avant un produit
+    # La valorisation peut être une catégorie à part
+  }
+
+  public function supprimer_produit(){
+
+  }
+
+  /***** Gestion des catégories *****/
+  public function creer_categorie(){
+
+  }
+
+  public function assigner_categorie(){
+    # Permet de mettre un produit dans une catégorie
+  }
+
+  public function changer_categorie(){
+    # Change le produit de catégorie
+  }
+
+  /***** Gestion des ventes *****/
+  public function afficher_ventes(){
+    # Affichage de tout l'historique des ventes
+  }
+}
+ ?>
