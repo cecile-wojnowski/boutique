df750211942b3ed162c646ca4d285d86be5abb94
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-17 10:42:02 +0200

    titre - barre de recherche - navbar

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<header>
+  <h1 class="logo"> Boutique en ligne </h1>
+  <p class="p_connexion">
+    <a class="link_header" href="connexion.php"> Connexion </a> ou <a class="link_header" href="inscription.php">créer un compte</a> </p>
+
+<!-- Barre de recherche -->
+
+   <div class="nav-wrapper">
+     <form>
+       <div class="input-field">
+         <input id="search" type="search" required>
+         <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+         <i class="material-icons">close</i>
+       </div>
+     </form>
+   </div>
+
+
+ <nav>
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href=""> Catégorie 1</a></li>
+        <li><a href="">Catégorie 2</a></li>
+        <li><a href="">Catégorie 3</a></li>
+      </ul>
+    </div>
+  </nav>
+
+</header>
--- index.php
+++ index.php
@@ -3,18 +3,18 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
   <body>
-    <header>
-
-    </header>
+    <?php include('includes/header.php'); ?>
 
     <main>
 
     </main>
 
-    <footer>
-
-    </footer>
+    <?php include('includes/footer.php'); ?>
   </body>
 </html>
