d85c86494e1ac28fc71b5c8003a69b3902f0326d
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-17 13:20:55 +0200

    Cartes

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
               </div>
               <div class="card-content">
                 <p> Noisettes </p>
+                <p> Prix </p>
               </div>
             </div>
           </div>
@@ -41,7 +42,8 @@
                 <a href="#"> <img src="img/nuts-mixed.jpg"> </a>
               </div>
               <div class="card-content">
-                <p>Mélange de fruits à coque.</p>
+                <p>Mélange de fruits à coque</p>
+                <p> Prix </p>
               </div>
             </div>
           </div>
@@ -52,7 +54,8 @@
                 <a href="#"> <img src="img/pistachio.jpg"> </a>
               </div>
               <div class="card-content">
-                <p>Pistaches.</p>
+                <p>Pistaches</p>
+                <p> Prix </p>
               </div>
             </div>
           </div>
@@ -63,7 +66,8 @@
                 <a href="#"> <img src="img/garlic.jpg"></a>
               </div>
               <div class="card-content">
-                <p>Ail.</p>
+                <p>Ail</p>
+                <p> Prix </p>
               </div>
             </div>
           </div>
@@ -71,7 +75,58 @@
 
       </section>
 
+      <!-- Section 2 -->
       <section>
+        <h3> Promotions </h3>
+        <div class="row">
+          <div class="col s3 m3">
+            <div class="card">
+              <div class="card-image">
+                <a href="#"> <img src="img/peppers.jpg"> </a>
+              </div>
+              <div class="card-content">
+                <p> Poivrons </p>
+                <p> Prix </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s3 m3">
+            <div class="card">
+              <div class="card-image">
+                <a href="#"> <img src="img/pepper-mix.jpg"> </a>
+              </div>
+              <div class="card-content">
+                <p>Mélange de baies (poivre)</p>
+                <p> Prix </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s3 m3">
+            <div class="card">
+              <div class="card-image">
+                <a href="#"> <img src="img/pumpkin2.jpg"> </a>
+              </div>
+              <div class="card-content">
+                <p>Courges</p>
+                <p> Prix </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s3 m3">
+            <div class="card">
+              <div class="card-image">
+                <a href="#"> <img src="img/tomatoes2.jpg"></a>
+              </div>
+              <div class="card-content">
+                <p>Tomates</p>
+                <p> Prix </p>
+              </div>
+            </div>
+          </div>
+        </div>
 
       </section>
 
