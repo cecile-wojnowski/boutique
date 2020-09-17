0f2f548e1244c0ab398c5084584ff897bbd5f749
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-17 13:42:19 +0200

    footer + icones

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Une question ?</h5>
+                <p class="grey-text text-lighten-4"><i class="material-icons">email</i> Ecrivez-nous.</p>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+              <div class="row">
+                Boutique en ligne
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-facebook-square"></i>
+            </div>
+
+            <div class="row">
+              Développé par Maxime Siegl et Cécile Wojnowski
+
+              <i class="fab fa-cc-mastercard"></i>
+              <i class="fab fa-cc-amex"></i>
+              <i class="fab fa-cc-paypal"></i>
+              <i class="fab fa-cc-visa"></i>
+            </div>
+
+            </div>
+          </div>
+        </footer>
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/eaf570753d.js" crossorigin="anonymous"></script>
   </head>
   <body>
     <?php include('includes/header.php'); ?>
@@ -72,7 +73,6 @@
             </div>
           </div>
         </div>
-
       </section>
 
       <!-- Section 2 -->
