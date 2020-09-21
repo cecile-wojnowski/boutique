458adcef3fb85921e2644252f2170fb57c5e8a8b
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-21 16:39:56 +0200

    Ajout de liens dans le header

--- includes/header.php
+++ includes/header.php
@@ -8,24 +8,24 @@
     </div>
 
     <div class="row">
-      <h1 class="logo col m2"> Boutique en ligne </h1>
+      <h1 class="logo col m2"> <a href="index.php"> Boutique en ligne </a></h1>
 
       <!-- Barre de recherche -->
       <div class="col m3 offset-m5">
         <nav class=" nav_header grey lighten-3">
-       <div class="nav-wrapper">
-         <form>
-           <div class="input-field">
-             <input id="search" type="search" placeholder ="Rechercher..." required>
-             <label class="label-icon" for="search"><i class="material-icons grey-text text-darken-2">search</i></label>
-             <i class="material-icons">close</i>
-           </div>
-         </form>
-       </div>
+         <div class="nav-wrapper">
+           <form>
+             <div class="input-field">
+               <input id="search" type="search" placeholder ="Rechercher..." required>
+               <label class="label-icon" for="search"><i class="material-icons grey-text text-darken-2">search</i></label>
+               <i class="material-icons">close</i>
+             </div>
+           </form>
+         </div>
      </nav>
       </div>
       <!-- Bouton Panier -->
-      <a class="waves-effect waves-green btn grey lighten-3 grey-text text-darken-2"><i class="material-icons left">shopping_cart</i> Panier </a>
+      <a href="panier.php" class="waves-effect waves-green btn grey lighten-3 grey-text text-darken-2"><i class="material-icons left">shopping_cart</i> Panier </a>
 
    </div>
 
