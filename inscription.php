81753581a879db34603c30091f97f320612575ca
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-22 11:03:55 +0200

    Centrage du formulaire et ajout d'un titre

--- inscription.php
+++ inscription.php
@@ -2,7 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Inscription</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
@@ -12,41 +12,41 @@
   </head>
   <body>
     <?php include('includes/header.php'); ?>
-
-<div class="row">
-    <form class="col s8 m8 offset-s3 offset-m3">
-      <div class="row">
-        <div class="input-field col m8 s8">
-          <input id="first_name" type="text" class="validate">
-          <label for="first_name">Prénom</label>
-        </div>
+    <h2 class="h2_form"> Créer un compte </h2>
+    <div class="row">
+        <form id="form_inscription" class="col s8 m8 offset-s3 offset-m3">
+          <div class="row">
+            <div class="input-field col m8 s8">
+              <input id="first_name" type="text" class="validate">
+              <label for="first_name">Prénom</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col m8 s8">
+              <input id="last_name" type="text" class="validate">
+              <label for="last_name">Nom</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col m8 s8">
+              <input id="email" type="email" class="validate">
+              <label for="email">Email</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col m8 s8">
+              <input id="password" type="password" class="validate">
+              <label for="password">Mot de passe </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col m8 s8">
+              <input id="password" type="password" class="validate">
+              <label for="password"> Confirmation du mot de passe </label>
+            </div>
+          </div>
+        </form>
       </div>
-      <div class="row">
-        <div class="input-field col m8 s8">
-          <input id="last_name" type="text" class="validate">
-          <label for="last_name">Nom</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col m8 s8">
-          <input id="email" type="email" class="validate">
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col m8 s8">
-          <input id="password" type="password" class="validate">
-          <label for="password">Mot de passe </label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col m8 s8">
-          <input id="password" type="password" class="validate">
-          <label for="password"> Confirmation du mot de passe </label>
-        </div>
-      </div>
-    </form>
-  </div>
 <?php include('includes/footer.php'); ?>
 </body>
 </html>
