faca0b1f59aec45fc667236a393bac2774567a3c
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-22 11:42:15 +0200

    Centrage du titre et du bouton + padding bouton

--- inscription.php
+++ inscription.php
@@ -12,9 +12,14 @@
   </head>
   <body>
     <?php include('includes/header.php'); ?>
-    <h2 class="h2_form"> Créer un compte </h2>
+
     <div class="row">
         <form id="form_inscription" class="col s8 m8 offset-s3 offset-m3">
+          <div class="row">
+            <div class="col s4 m4 offset-s2 offset-m2">
+              <h2 > Créer un compte </h2>
+            </div>
+          </div>
           <div class="row">
             <div class="input-field col m8 s8">
               <input id="first_name" type="text" class="validate">
@@ -45,6 +50,13 @@
               <label for="password"> Confirmation du mot de passe </label>
             </div>
           </div>
+
+          <div class="row">
+            <div class="input-field col m3 s3 offset-m3 offset-s3">
+              <a id="bouton_inscription" class="waves-effect waves-green btn grey darken-4 lighten-3 white-text">
+                S'incrire</a>
+              </div>
+            </div>
         </form>
       </div>
 <?php include('includes/footer.php'); ?>
