4fd5da5864186e9007447769fbf16f1fd7e1ea44
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-09-22 13:49:21 +0200

    Ajout de conditions de vérification aux setters

--- classes/Produits.php
+++ classes/Produits.php
@@ -7,6 +7,7 @@ soient accessibles et modifiables ?
 */
 class Produits
 {
+  private $id;
   private $nom;
   private $prix;
   private $description;
@@ -24,24 +25,51 @@ class Produits
   }
   # Setters : servent à modifier les attributs en dehors de la classe
   # peut être faudra-t-il ajouter une requête sql pour que la modification se fasse dans la bdd
+  public function setId($id){
+    $id = (int) $id;
+   // On vérifie si ce nombre est bien strictement positif.
+   if ($id > 0)
+   {
+     // Si c'est le cas, c'est tout bon, on assigne la valeur à l'attribut correspondant.
+     $this->_id = $id;
+   }
+ }
+  }
   public function setNom($nom){
-    return $this->nom = $nom;
+    if (is_string($nom))
+    {
+      $this->nom = $nom;
+    }
   }
   public function setPrix($prix){
-    return $this->prix = $prix;
+    $prix = (int) $prix;
+   if ($prix > 0)
+   {
+     $this->prix = $prix;
+   }
   }
   public function setDescription(){
-    return $this->description = $description;
+    if (is_string($description))
+    {
+      $this->description = $description;
+    }
   }
+  public function setQuantite_stock($quantite_stock){
+    $quantite_stock = (int) $quantite_stock;
+
+   if ($quantite_stock >= 0) # Peut être égal à 0, mais ne peut pas être négatif
+   {
+     $this->quantite_stock = $quantite_stock;
+   }
+  }
+  # Conditions à rajouter pour ces setters ?
   public function setImage(){
     return $this->image = $image;
   }
   public function setDate_ajout($date_ajout){
     return $this->date_ajout = $date_ajout;
   }
-  public function setQuantite_stock($quantite_stock){
-    return $this->quantite_stock = $quantite_stock;
-  }
+
   public function setEst_valorise($est_valorise){
     return $this->est_valorise = $est_valorise;
   }
