ad104640f100d5afe8b24114b6f4a870fb012836
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-05 15:03:13 +0200

    Ajout id + hydratation + getters & setters

--- classes/Categorie.php
+++ classes/Categorie.php
@@ -1,8 +1,10 @@
 <?php
 class Categorie
 {
+  private $id;
   private $nom;
-  private $liste_produit; # Tableau contenant les produits de la catégorie
+  # filtrer les produits appartenant à la catégorie grâce à la classe ?
+  private $liste_produit = []; # Tableau contenant les produits de la catégorie
   private $liste_sous_categories; # Tableau contenant les sous-catégories liées à la catégorie
 
   public function afficher_produits(){
@@ -12,5 +14,45 @@ class Categorie
   public function afficher_sous_categories(){
     # Déroule les sous-catégories liées à la catégorie
   }
+
+  // Hydratation
+  public function hydrate(array $donnees)
+  {
+    if (isset($donnees['id']))
+    {
+      $this->setId($donnees['id']);
+    }
+    if (isset($donnees['nom']))
+    {
+      $this->setNom($donnees['nom']);
+    }
+  }
+
+  // Setters
+  public function setId($id){
+    $id = (int) $id;
+    if ($id > 0)
+    {
+     $this->id = $id;
+    }
+  }
+  public function setNom($nom){
+    if (is_string($nom))
+    {
+      $this->nom = $nom;
+    }
+  }
+
+  // Getters
+  public function id(){
+    return $this->id;
+  }
+  public function nom(){
+    return $this->nom;
+  }
+  public function prix(){
+    return $this->prix;
+  }
+
 }
  ?>
