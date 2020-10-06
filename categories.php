70edf3d3e528e3bae52338f913d7fcae65fb1d8c
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-06 14:41:41 +0200

    Affiche les sous-catégories liées à la catégorie

--- categories.php
+++ categories.php
@@ -5,6 +5,7 @@
   include('classes/Produit.php');
   include('classes/Panier.php');
   include('classes/Categorie.php');
+  include('classes/SousCategorie.php')
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -24,11 +25,29 @@
         $categorie = new Categorie();
         $categorie->recuperer_produits($db, $id_categorie);
 
+
+        $sousCategorie = new SousCategorie();
+        $sousCategorie->recuperer_produits($db, $id_categorie);
+        $sousCategorie->afficher_produits($db);
+
         # Permet l'affichage du nom de la catégorie
         $query = $db->prepare("SELECT nom_header FROM categories WHERE id = '$id_categorie'");
         $query->execute();
         while ($donnees = $query->fetch(PDO::FETCH_ASSOC)){?>
-        <h2>  <?= $donnees['nom_header']; ?>  </h2> <?php } ?>
+          <h2>  <?= $donnees['nom_header']; ?>  </h2>
+          <?php
+        } ?>
+
+        <?php
+        # Afficher les sous-catégories liées à la catégorie
+        $query = $db->prepare("SELECT * FROM sous_categories WHERE id_categorie = '$id_categorie'");
+        $query->execute();
+        while ($donnees = $query->fetch(PDO::FETCH_ASSOC)){
+           echo $donnees['nom'] . " ";
+
+        } ?>
+
+
 
         <!-- Affiche les produits -->
         <div class='row'>
@@ -39,18 +58,7 @@
         header('Location:index.php'); # Redirige si on tente d'aller sur cette page sans avoir choisi une catégorie
       } ?>
 
-
-
-
-
-
-
-
-
-
-
     </main>
-
     <?php include 'includes/footer.php'; ?>
   </body>
 </html>
