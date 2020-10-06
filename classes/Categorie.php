e03e393f83ae3b9779009a22bdb66c5c0a6c3721
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-06 10:37:22 +0200

    Stockage de l'id des produits appartenant à une catégorie déterminée

--- classes/Categorie.php
+++ classes/Categorie.php
@@ -4,8 +4,25 @@ class Categorie
   private $id;
   private $nom;
   # filtrer les produits appartenant à la catégorie grâce à la classe ?
-  private $liste_produit = []; # Tableau contenant les produits de la catégorie
+  private $liste_produits = []; # Tableau contenant les produits de la catégorie
   private $liste_sous_categories; # Tableau contenant les sous-catégories liées à la catégorie
+
+  public function recuperer_produits($db, $id_categorie){
+    # Remplir la liste de produits avec tous les produits ayant le bon id dans la bdd
+    $query = $db->prepare("SELECT id, nom FROM produits WHERE id_categorie = '$id_categorie'");
+    $query->execute();
+
+    while ($donnees = $query->fetch(PDO::FETCH_ASSOC)){
+      echo $donnees['id'] . " ";
+      echo $donnees['nom'] . "<br>";
+
+      # On place les id des produits dans le tableau
+      $this->liste_produits[] = $donnees['id'];
+    }
+
+    var_dump($this->liste_produits);
+
+  }
 
   public function afficher_produits(){
     # Permet de montrer les produits appartenant à la catégorie
@@ -43,6 +60,7 @@ class Categorie
     }
   }
 
+
   // Getters
   public function id(){
     return $this->id;
@@ -52,6 +70,9 @@ class Categorie
   }
   public function prix(){
     return $this->prix;
+  }
+  public function liste_produits(){
+    return $this->liste_produits;
   }
 
 }
