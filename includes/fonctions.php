d2681c74f5a934cf8c2a81e913c45d4ebd95c11d
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-07 13:43:15 +0200

    Création de la fonction afficher_promotions

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -3,4 +3,14 @@
         session_destroy();
         header('Location:index.php');
     }
+
+    function afficher_promotions($db){
+      $query = $db->prepare("SELECT * FROM produits WHERE valorisation = 1");
+      $query->execute();
+
+      while ($donnees = $query->fetch(PDO::FETCH_ASSOC)){
+        echo "<a href='produit.php?id=" . $donnees['id']. "'>" . $donnees['nom'] . "</a>";
+      }
+
+    }
 ?>
