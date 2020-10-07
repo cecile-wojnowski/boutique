bd3e66b9c6bc675a484ba8dabd879d326d5b02ab
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-07 13:57:57 +0200

    Affichage des derniers produits ajoutés

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -9,8 +9,6 @@
       $query->execute();
 
       while ($donnees = $query->fetch(PDO::FETCH_ASSOC)){
-        #echo "<a href='produit.php?id=" . $donnees['id']. "'>" . $donnees['nom'] . "</a>";
-
         echo
           "<div class='col s2 m3'>
             <div class='card'>
@@ -28,6 +26,29 @@
               "</div>
           </div>";
       }
+    }
 
+    function afficher_nouveautes($db){
+      $query = $db->prepare("SELECT * FROM produits ORDER BY date_ajout DESC LIMIT 8");
+      $query->execute();
+
+      while ($donnees = $query->fetch(PDO::FETCH_ASSOC)){
+        echo
+          "<div class='col s2 m3'>
+            <div class='card'>
+              <div class='card-image'>
+              <a href='produit.php?id=" . $donnees['id'] . "'>" .
+                "<img src='img/" . $donnees['image']. "'></a>
+              </div>
+            </div>
+
+            <div class='card-content'>"
+                . $donnees["nom"] . "<br>"
+                . "<span class='ancien_prix'>" . $donnees["prix"] . " " . "euros" . "</span>" . "<br>"
+                . $donnees["prix_solde"] . " " . "euros".
+
+              "</div>
+          </div>";
+      }
     }
 ?>
