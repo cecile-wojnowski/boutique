a3b02d1afc0b28281b38e55ae9cb69a5dcae2bb9
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-12 13:24:30 +0200

    Réactivation du lien fontawesome

--- includes/links.php
+++ includes/links.php
@@ -2,4 +2,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="css/style.css">
-<!-- <script src="https://kit.fontawesome.com/eaf570753d.js" crossorigin="anonymous"></script> -->
+<script src="https://kit.fontawesome.com/eaf570753d.js" crossorigin="anonymous"></script>
