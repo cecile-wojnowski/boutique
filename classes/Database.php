1c422707c92750e036cf56ca910b02f7437523e4
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-13 14:44:24 +0200

    port 3308

--- classes/Database.php
+++ classes/Database.php
@@ -4,7 +4,7 @@
         private $pdo;
 
         public function __construct($login, $password, $database_name, $host = 'localhost'){
-            $this->pdo = new PDO("mysql:host=$host; port=3306; dbname=$database_name", "$login", "$password");
+            $this->pdo = new PDO("mysql:host=$host; port=3308; dbname=$database_name", "$login", "$password");
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
         }
