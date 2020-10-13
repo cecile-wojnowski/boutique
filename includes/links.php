53fbd70ca1b6780ef26ec3a971fa56e5b8fb35cb
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-13 12:03:45 +0200

    Correction } manquant dans css

--- includes/links.php
+++ includes/links.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<link rel="stylesheet" href="css/style.css" type="text/css"/>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="css/style.css">
 <script src="https://kit.fontawesome.com/eaf570753d.js" crossorigin="anonymous"></script>
