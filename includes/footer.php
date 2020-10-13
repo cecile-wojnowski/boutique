2fd273cd01f93e4fee1a7097da19036c83a4d03e
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-10-13 15:05:04 +0200

    Avancement CSS footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,26 +3,30 @@
             <div class="row">
               <div class="col l6 s12">
                 <h5 class="white-text">Une question ?</h5>
-                <p class="grey-text text-lighten-4"><i class="material-icons">email</i> Ecrivez-nous.</p>
+                <p><a href="mailto:cecile.wojnowski@laplateforme.io">
+                  <i class="material-icons">email</i> <span class="text-align">Ecrivez-nous.</span></a></p>
               </div>
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
-              <div class="row">
+              <div class="row" id="icones_footer">
                 Boutique en ligne
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-facebook-square"></i>
-            </div>
+                <div>
+                  <i class="fab fa-instagram"></i>
+                  <i class="fab fa-twitter"></i>
+                  <i class="fab fa-facebook-square"></i>
+                </div>
+              </div>
 
-            <div class="row">
+            <div class="row" id="icones_footer">
               Développé par Maxime Siegl et Cécile Wojnowski
-
-              <i class="fab fa-cc-mastercard"></i>
-              <i class="fab fa-cc-amex"></i>
-              <i class="fab fa-cc-paypal"></i>
-              <i class="fab fa-cc-visa"></i>
+              <div>
+                <i class="fab fa-cc-mastercard"></i>
+                <i class="fab fa-cc-amex"></i>
+                <i class="fab fa-cc-paypal"></i>
+                <i class="fab fa-cc-visa"></i>
+              </div>
             </div>
 
             </div>
